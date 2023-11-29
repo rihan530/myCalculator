9f535a92bcb7f30410ed0ca08d4956fe4fd9f30b
rihan530 <renovatio530@gmail.com>
2023-11-29 14:05:17 +0900

    계산기 lv2

--- app/src/main/java/com/example/mycalculator/myCalculator1.kt
+++ app/src/main/java/com/example/mycalculator/myCalculator1.kt
@@ -9,22 +9,22 @@ fun main() {
     println("$num1 $operator $num2 = $result")
 }
 
-class Calculator(){
+class Calculator{
     fun plus(num1: Int, num2: Int): Int {
-            return num1 + num2
-        }
+        return num1 + num2
+    }
 
-        fun minus(num1: Int, num2: Int): Int {
-            return num1 - num2
-        }
+    fun minus(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
 
-        fun multiply(num1: Int, num2: Int): Int {
-            return num1 * num2
-        }
+    fun multiply(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
 
-        fun division(num1: Int, num2: Int): Int {
-            return num1 / num2
-        }
+    fun division(num1: Int, num2: Int): Int {
+        return num1 / num2
+    }
 }
 
 fun calculate(num1: Int, operator : String, num2 : Int): Int {
